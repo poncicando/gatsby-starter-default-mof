2b272f5999ac4e991b8539a556e2ec0792916963
poncicando <53867772+poncicando@users.noreply.github.com>
2021-02-19 15:17:53 +0900

    Add Changes For the Pages

--- src/pages/using-typescript.tsx
+++ src/pages/using-typescript.tsx
@@ -14,11 +14,10 @@ type DataProps = {
 const UsingTypescript: React.FC<PageProps<DataProps>> = ({ data, path }) => (
   <Layout>
     <SEO title="Using TypeScript" />
-    <h1>Gatsby supports TypeScript by default!</h1>
+    <h1>Test Typescript!</h1>
     <p>
       This means that you can create and write <em>.ts/.tsx</em> files for your
-      pages, components etc. Please note that the <em>gatsby-*.js</em> files
-      (like gatsby-node.js) currently don't support TypeScript yet.
+      pages, components etc. 
     </p>
     <p>
       For type checking you'll want to install <em>typescript</em> via npm and
